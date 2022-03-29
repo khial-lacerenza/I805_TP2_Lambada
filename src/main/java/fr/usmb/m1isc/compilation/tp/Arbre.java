c596f12e9056b9c7b9b1891f8291a400b4edaff4
Omar <omarkhial74@gmail.com>
2022-03-29 16:30:16 +0200

    trying to fix blank parenthesis

--- src/main/java/fr/usmb/m1isc/compilation/tp/Arbre.java
+++ src/main/java/fr/usmb/m1isc/compilation/tp/Arbre.java
@@ -63,21 +63,26 @@ public class Arbre {
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        sb.append(valeur).append(" ");
-        if (filsGauche != null) {
-            if (filsGauche.getFilsGauche() != null && filsGauche.getFilsDroit() != null) {
-                sb.append("(").append(filsGauche).append(")");
-            }else
-            {
-                sb.append(filsGauche);
+        if(valeur != "")
+        {
+            sb.append(valeur).append(" ");
+            if (filsGauche != null) {
+                if (filsGauche.getFilsGauche() != null && filsGauche.getFilsDroit() != null) {
+                    if(filsGauche.getFilsGauche().getValeur() != " " && filsGauche.getFilsDroit().getValeur() != " " )
+                        sb.append("(").append(filsGauche).append(")");
+                }else
+                {
+                    sb.append(filsGauche);
+                }
             }
-        }
-        if (filsDroit != null) {
-            if (filsDroit.getFilsGauche() != null && filsDroit.getFilsDroit() != null) {
-                sb.append("(").append(filsDroit).append(")");
-            }else
-            {
-                sb.append(filsDroit);
+            if (filsDroit != null) {
+                if ((filsDroit.getFilsGauche() != null && filsDroit.getFilsDroit() != null)) {
+                    if((filsDroit.getFilsGauche().getValeur() != " " && filsDroit.getFilsDroit().getValeur() != " " ))
+                        sb.append("(").append(filsDroit).append(")");
+                }else
+                {
+                    sb.append(filsDroit);
+                }
             }
         }
         return sb.toString();
